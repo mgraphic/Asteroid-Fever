16f48f17e42c9aca93635f30bc872f85743e04c2
MARSK069 <keith.e.marshall.-nd@ddisney.com>
2021-11-27 10:26:30 -0500

    Cleanup

--- src/scripts/sprites/ship.ts
+++ src/scripts/sprites/ship.ts
@@ -10,7 +10,7 @@ export class Ship extends AbstractSprite {
     private nosePosition: Coordinates2D;
     private velocityPosition: Coordinates2D;
     private inMotion: boolean = false;
-    private radius: number = 15; //15;
+    private radius: number = 15;
     protected angle: number = 0;
     private hidden: boolean = false;
     private display: boolean = true;
